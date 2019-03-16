bba88bb777040d70e95eed71bd70e61c9744ab23
Pablo Cantero <pablohstc@gmail.com>
2019-03-16 16:24:05 -0400

    :lipstick:

--- ops/bin/ops.ts
+++ ops/bin/ops.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/cdk');
-import { OpsStack } from '../lib/ops-stack';
+import 'source-map-support/register'
+import cdk = require('@aws-cdk/cdk')
+import { OpsStack } from '../lib/ops-stack'
 
-const app = new cdk.App();
-new OpsStack(app, 'OpsStack');
-app.run();
+const app = new cdk.App()
+new OpsStack(app, 'OpsStack')
+app.run()
--- ops/lib/ops-stack.ts
+++ ops/lib/ops-stack.ts
@@ -1,25 +1,30 @@
-import ec2 = require('@aws-cdk/aws-ec2');
-import ecs = require('@aws-cdk/aws-ecs');
-import cdk = require('@aws-cdk/cdk');
-
+import ec2 = require('@aws-cdk/aws-ec2')
+import ecs = require('@aws-cdk/aws-ecs')
+import cdk = require('@aws-cdk/cdk')
 
 export class OpsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-load-balanced-service/index.ts
     // Create VPC and Fargate Cluster
     // NOTE: Limit AZs to avoid reaching resource quotas
-    const vpc = new ec2.VpcNetwork(this, 'MyVpc', { maxAZs: 2 });
-    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+    const vpc = new ec2.VpcNetwork(this, 'MyVpc', { maxAZs: 2 })
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc })
 
     // Instantiate Fargate Service with just cluster and image
-    const fargateService = new ecs.LoadBalancedFargateService(this, "FargateService", {
-      cluster,
-      image: ecs.ContainerImage.fromDockerHub("amazon/amazon-ecs-sample"),
-    });
+    const fargateService = new ecs.LoadBalancedFargateService(
+      this,
+      'FargateService',
+      {
+        cluster,
+        image: ecs.ContainerImage.fromDockerHub('amazon/amazon-ecs-sample')
+      }
+    )
 
     // Output the DNS where you can access your service
-    new cdk.Output(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.dnsName });
+    new cdk.Output(this, 'LoadBalancerDNS', {
+      value: fargateService.loadBalancer.dnsName
+    })
   }
 }
