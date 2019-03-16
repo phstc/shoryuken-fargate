1488e123f399f48701c4659da3c85f1a56b24bc5
Pablo Cantero <pablohstc@gmail.com>
2019-03-16 16:21:24 -0400

    Copy & pasta of fargate-load-balanced-service
    See https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-load-balanced-service/index.ts

--- ops/lib/ops-stack.ts
+++ ops/lib/ops-stack.ts
@@ -1,9 +1,25 @@
+import ec2 = require('@aws-cdk/aws-ec2');
+import ecs = require('@aws-cdk/aws-ecs');
 import cdk = require('@aws-cdk/cdk');
+
 
 export class OpsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-load-balanced-service/index.ts
+    // Create VPC and Fargate Cluster
+    // NOTE: Limit AZs to avoid reaching resource quotas
+    const vpc = new ec2.VpcNetwork(this, 'MyVpc', { maxAZs: 2 });
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+
+    // Instantiate Fargate Service with just cluster and image
+    const fargateService = new ecs.LoadBalancedFargateService(this, "FargateService", {
+      cluster,
+      image: ecs.ContainerImage.fromDockerHub("amazon/amazon-ecs-sample"),
+    });
+
+    // Output the DNS where you can access your service
+    new cdk.Output(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.dnsName });
   }
 }
